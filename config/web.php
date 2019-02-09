c868d13fef74e3b1930cdbde66c77c7309b64b3b
Dzahkiev <egikalegi@gmail.com>
2019-02-09 23:35:41 +0300

    First admin template settings

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'My App Name',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
